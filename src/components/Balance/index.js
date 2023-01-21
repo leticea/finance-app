e66f86b819f96c8498a5bf5aa05c118a82e9b477
leticea <nascimento.leticeah@gmail.com>
2023-01-21 19:00:50 -0300

    criando editando o estilo

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -11,14 +11,14 @@ export default function Balance() {
 
       <View style={styles.item}>
         <Text style={styles.itemTitle}>Saldo</Text>
-        <View>
+        <View style={styles.content}>
           <Text style={styles.currencySymbol}>R$</Text>
           <Text style={styles.balance}>15.000,00</Text>
         </View>
       </View>
       <View style={styles.item}>
         <Text style={styles.itemTitle}>Gastos</Text>
-        <View>
+        <View style={styles.content}>
           <Text style={styles.currencySymbol}>R$</Text>
           <Text style={styles.balance}>480,00</Text>
         </View>
@@ -31,5 +31,20 @@ export default function Balance() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FFF',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingStart: 18,
+    paddingEnd: 18,
+    marginTop: -24,
+    marginStart: 14,
+    marginEnd: 14,
+    borderRadius: 4,
+    paddingTop: 22,
+    paddingBottom: 22,
+    zIndex: 99
+  },
+  itemTitle: {
+    fontSize: 20,
+    color: '#DADADA'
   }
 })
