3f8e14c5af53e7a843a6c07be8ea621677cee964
leticea <nascimento.leticeah@gmail.com>
2023-01-21 20:33:16 -0300

    criando estado para ocultar ou mostrar os valores

--- src/components/Movements/index.js
+++ src/components/Movements/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,6 +7,8 @@ import {
 } from "react-native";
 
 export default function Movements({ data }) {
+  const [showValue, setShowValue] = useState(false);
+
   return (
     <TouchableOpacity style={styles.container}>
       <Text style={styles.date}>{data.date}</Text>
@@ -14,9 +16,17 @@ export default function Movements({ data }) {
       <View style={styles.content}>
         <Text style={styles.label}>{data.label}</Text>
 
-        <Text style={data.type === 1 ? styles.value : styles.expenses}>
-          {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
-        </Text>
+        {showValue ? (
+          <Text style={data.type === 1 ? styles.value : styles.expenses}>
+            {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
+          </Text>
+        ) : (
+          <View style={styles.skeleton}>
+
+          </View>
+        )}
+
+
       </View>
     </TouchableOpacity>
   );
@@ -52,5 +62,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#e74c3c',
     fontWeight: 'bold'
+  },
+  skeleton: {
+    marginTop: 6,
+    width: 80,
+    height: 10,
+    backgroundColor: '#DADADA',
+    borderRadius: 8
   }
 })
