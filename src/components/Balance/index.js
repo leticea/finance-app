30ac6b58df8987175da7973e33144abd3c2e63d9
leticea <nascimento.leticeah@gmail.com>
2023-01-21 20:04:05 -0300

    criando a listagem de dados

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -5,25 +5,23 @@ import {
   StyleSheet,
 } from "react-native";
 
-export default function Balance() {
+export default function Balance({ saldo, gastos }) {
   return (
     <View style={styles.container}>
-
       <View style={styles.item}>
         <Text style={styles.itemTitle}>Saldo</Text>
         <View style={styles.content}>
           <Text style={styles.currencySymbol}>R$</Text>
-          <Text style={styles.balance}>15.000,00</Text>
+          <Text style={styles.balance}>{saldo}</Text>
         </View>
       </View>
       <View style={styles.item}>
         <Text style={styles.itemTitle}>Gastos</Text>
         <View style={styles.content}>
           <Text style={styles.currencySymbol}>R$</Text>
-          <Text style={styles.balance}>480,00</Text>
+          <Text style={styles.expenses}>{gastos}</Text>
         </View>
       </View>
-
     </View>
   );
 };
@@ -46,5 +44,21 @@ const styles = StyleSheet.create({
   itemTitle: {
     fontSize: 20,
     color: '#DADADA'
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  currencySymbol: {
+    color: '#DADADA',
+    marginRight: 6
+  },
+  balance: {
+    fontSize: 22,
+    color: '#2ecc71'
+  },
+  expenses: {
+    fontSize: 22,
+    color: '#e74c3c'
   }
-})
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "../../components/Header";
 import Balance from "../../components/Balance";
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto conta luz',
+    value: '300,90',
+    date: '17/09/2022',
+    type: 0 //despesas
+  },
+  {
+    id: 2,
+    label: 'Pix cliente X',
+    value: '2.500,00',
+    date: '17/09/2022',
+    type: 0 //receita / entradas
+  },
+  {
+    id: 3,
+    label: 'Salário',
+    value: '7.500,00',
+    date: '17/09/2022',
+    type: 0 //receita / entradas
+  },
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="Maria" />
-      <Balance />
+      <Balance saldo="9.250.90" gastos="-527.00" />
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={ ({ item }) => <Text>TESTE</Text>}
+      />
     </View>
   );
 }
@@ -16,4 +50,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fafafa',
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
+  }
 });
